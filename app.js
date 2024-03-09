18bdc6e81238ff47e1637e624f707a09351190bb
harmony <harmony.rosenberg94@gmail.com>
2024-03-09 12:56:17 -0600

    phase 1 and 2

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require('express');
+require('express-async-errors');
 const app = express();
+app.use(express.json());
+
+const logMiddleware = (req, res, next) => {
+  console.log(req.method)
+  console.log(req.url)
+  res.on('finish', () => {
+    console.log(res.statusCode)
+  });
+  next()
+}
+app.use(logMiddleware)
 
 // For testing purposes, GET /
 app.get('/', (req, res) => {
@@ -16,8 +28,26 @@ app.post('/test-json', (req, res, next) => {
 
 // For testing express-async-errors
 app.get('/test-error', async (req, res) => {
-  throw new Error("Hello World!")
+  throw new Error("Hello World Error!")
 });
+
+
+let errorMiddleWare = (req, res, next) => {
+  let err = new Error("The requested resource couldn't be found.")
+  err.statusCode = 404
+  next(err)
+}
+
+app.use(errorMiddleWare)
+
+app.use((err, req, res, next) => {
+  let statusCode = err.statusCode || 500
+  res.status(statusCode)
+  let message = {
+    message: err.message
+  }
+  res.json(message)
+})
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
